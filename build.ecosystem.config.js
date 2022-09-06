390c421039d346118d2cd2690b084a978e5bd5a9
paulhorstmann <paulluishorstmann@gmail.com>
2022-09-06 17:28:27 +0200

    build

--- build.ecosystem.config.js
+++ build.ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
     apps: [{
         name: "TU-Build",
         script: "./dist/app.js",
-        instances: 1,
         watch: false,
         restart_delay: 5000,
         max_memory_restart: "2G"
